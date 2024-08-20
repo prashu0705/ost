20dc8f649ea3b12d35a1ba972a0e3c937b181b99
prashu0705 <prashu0705@gmail.com>
2024-08-20 19:35:19 +0530

    program to calculate sum and average of 2 numbers

---
+++ exp3/sum_average.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int main () {
+int a, b;
+int sum = 0;
+int average = 0;
+cout<<"enter the values of a and b";
+cin>>a>>b;
+sum = a+b;
+average = sum/2;
+cout<<sum;
+cout<<average;
+}
